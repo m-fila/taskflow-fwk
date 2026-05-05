277bbac869c444b35e64012d72d3bf3783dea522
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-05-05 23:11:30 +0200

    run algs for average time with no variance

--- src/cpu_cruncher.cpp
+++ src/cpu_cruncher.cpp
@@ -169,11 +169,8 @@ namespace mockup {
 
   void CPUCruncher::operator()() {
 
-    auto distribution =
-        std::normal_distribution<runtime_duration::rep>{ m_duration_average.count(), m_duration_stddev.count() };
-    auto       random_duration = runtime_duration( std::abs( distribution( m_random ) ) );
-    const auto sleep_duration  = m_sleep_fraction * random_duration;
-    const auto work_duration   = ( 1 - m_sleep_fraction ) * random_duration;
+    const auto sleep_duration  = m_sleep_fraction * m_duration_average;
+    const auto work_duration   = ( 1 - m_sleep_fraction ) * m_duration_average;
     if ( m_sleep_fraction > 0 ) { std::this_thread::sleep_for( sleep_duration ); }
     if ( m_sleep_fraction < 1 ) { m_cruncher->crunch( work_duration ); }
   }
